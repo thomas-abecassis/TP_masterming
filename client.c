e62d760ef368e88f213a40d3e27bf0778cde7ffc
Thomas Abecassis <abecasst@f212-13.e-im2ag.ujf-grenoble.fr>
2021-04-16 14:40:15 +0200

    first commit

---
+++ client.c
@@ -0,0 +1,78 @@
+/******************************************************************************/
+/*			Application: ...					*/
+/******************************************************************************/
+/*									      */
+/*			 programme  CLIENT				      */
+/*									      */
+/******************************************************************************/
+/*									      */
+/*		Auteurs : ... 					*/
+/*									      */
+/******************************************************************************/	
+
+
+#include <stdio.h>
+#include <curses.h> 		/* Primitives de gestion d'ecran */
+#include <sys/signal.h>
+#include <sys/wait.h>
+#include<stdlib.h>
+
+#include "fon.h"   		/* primitives de la boite a outils */
+
+#define SERVICE_DEFAUT "1111"
+#define SERVEUR_DEFAUT "127.0.0.1"
+
+void client_appli (char *serveur, char *service);
+
+
+/*****************************************************************************/
+/*--------------- programme client -----------------------*/
+
+int main(int argc, char *argv[])
+{
+
+	char *serveur= SERVEUR_DEFAUT; /* serveur par defaut */
+	char *service= SERVICE_DEFAUT; /* numero de service par defaut (no de port) */
+
+
+	/* Permet de passer un nombre de parametre variable a l'executable */
+	switch(argc)
+	{
+ 	case 1 :		/* arguments par defaut */
+		  printf("serveur par defaut: %s\n",serveur);
+		  printf("service par defaut: %s\n",service);
+		  break;
+  	case 2 :		/* serveur renseigne  */
+		  serveur=argv[1];
+		  printf("service par defaut: %s\n",service);
+		  break;
+  	case 3 :		/* serveur, service renseignes */
+		  serveur=argv[1];
+		  service=argv[2];
+		  break;
+    default:
+		  printf("Usage:client serveur(nom ou @IP)  service (nom ou port) \n");
+		  exit(1);
+	}
+
+	/* serveur est le nom (ou l'adresse IP) auquel le client va acceder */
+	/* service le numero de port sur le serveur correspondant au  */
+	/* service desire par le client */
+	
+	client_appli(serveur,service);
+}
+
+/*****************************************************************************/
+void client_appli (char *serveur,char *service)
+
+/* procedure correspondant au traitement du client de votre application */
+
+{
+  
+
+/* a completer .....  */
+
+ }
+
+/*****************************************************************************/
+
---
+++ serveur.c
@@ -0,0 +1,67 @@
+/******************************************************************************/
+/*			Application: ....			              */
+/******************************************************************************/
+/*									      */
+/*			 programme  SERVEUR 				      */
+/*									      */
+/******************************************************************************/
+/*									      */
+/*		Auteurs :  ....						      */
+/*		Date :  ....						      */
+/*									      */
+/******************************************************************************/	
+
+#include<stdio.h>
+#include <curses.h>
+
+#include<sys/signal.h>
+#include<sys/wait.h>
+#include<stdlib.h>
+
+#include "fon.h"     		/* Primitives de la boite a outils */
+
+#define SERVICE_DEFAUT "1111"
+
+void serveur_appli (char *service);   /* programme serveur */
+
+
+/******************************************************************************/	
+/*---------------- programme serveur ------------------------------*/
+
+int main(int argc,char *argv[])
+{
+
+	char *service= SERVICE_DEFAUT; /* numero de service par defaut */
+
+
+	/* Permet de passer un nombre de parametre variable a l'executable */
+	switch (argc)
+ 	{
+   	case 1:
+		  printf("defaut service = %s\n", service);
+		  		  break;
+ 	case 2:
+		  service=argv[1];
+            break;
+
+   	default :
+		  printf("Usage:serveur service (nom ou port) \n");
+		  exit(1);
+ 	}
+
+	/* service est le service (ou numero de port) auquel sera affecte
+	ce serveur*/
+	
+	serveur_appli(service);
+}
+
+
+/******************************************************************************/	
+void serveur_appli(char *service)
+
+	int socket = h_socket(AF_INET, SOCK_STREAM);
+
+}
+
+/******************************************************************************/	
+
